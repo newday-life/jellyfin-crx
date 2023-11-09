a08d3e471aec166d8cfe8ded465b5e9942ad3607
jackloves111 <89971817+jackloves111@users.noreply.github.com>
2023-11-09 20:43:14 +0800

    尝试修复wget访问HTTPS安全机制问题

--- script.sh
+++ script.sh
@@ -7,11 +7,11 @@ docker exec -it  $name rm -rf /jellyfin/jellyfin-web/jellyfin-crx/
 docker exec -it  $name mkdir -p /jellyfin/jellyfin-web/jellyfin-crx/
 
 # 下载所需文件到系统
-wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/css/style.css -O style.css  
-wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/common-utils.js -O common-utils.js  
-wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/jquery-3.6.0.min.js -O jquery-3.6.0.min.js  
-wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/md5.min.js -O md5.min.js  
-wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/content/main.js -O main.js
+wget --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/css/style.css -O style.css  
+wget --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/common-utils.js -O common-utils.js  
+wget --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/jquery-3.6.0.min.js -O jquery-3.6.0.min.js  
+wget --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/md5.min.js -O md5.min.js  
+wget --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/content/main.js -O main.js
 
 # 从系统复制文件到容器内
 docker cp style.css $name:/jellyfin/jellyfin-web/jellyfin-crx/
