dfb850be93038241f2cf769beb60383ca0441701
jackloves111 <89971817+jackloves111@users.noreply.github.com>
2023-11-09 08:24:08 +0800

    修复首次安装判断错误

--- script.sh
+++ script.sh
@@ -2,17 +2,18 @@
 
 read -p "请输入 Jellyfin 容器名称:" name
 
+# 在容器内创建文件夹
 docker exec -it  $name rm -rf /jellyfin/jellyfin-web/jellyfin-crx/
 docker exec -it  $name mkdir -p /jellyfin/jellyfin-web/jellyfin-crx/
 
-# 下载所需文件
+# 下载所需文件到系统
 wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/css/style.css -O style.css  
 wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/common-utils.js -O common-utils.js  
 wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/jquery-3.6.0.min.js -O jquery-3.6.0.min.js  
 wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/md5.min.js -O md5.min.js  
 wget https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/content/content/main.js -O main.js
 
-# 安置
+# 从系统复制文件到容器内
 docker cp style.css $name:/jellyfin/jellyfin-web/jellyfin-crx/
 docker cp common-utils.js $name:/jellyfin/jellyfin-web/jellyfin-crx/
 docker cp jquery-3.6.0.min.js $name:/jellyfin/jellyfin-web/jellyfin-crx/
@@ -36,13 +37,16 @@ function Installing() {
 	docker cp ./index.html $name:/jellyfin/jellyfin-web/
 }
 
+# 先复制一份到系统内
+docker cp $name:/jellyfin/jellyfin-web/index.html ./
+
 # 检查index.html是否包含jellyfin-crx
 if grep -q "jellyfin-crx" index.html; then
-    echo "Index.html 已经修改过！"
     docker cp $name:/jellyfin/jellyfin-web/bak/index.html ./
     Installing
+    echo "成功！Index.html 已重新修改！"
 else
-    echo "Index.html 首次安装！"
+    echo "成功！Index.html 首次安装！"
 	docker cp $name:/jellyfin/jellyfin-web/index.html ./
 	# 备份
 	docker exec -it  $name mkdir -p /jellyfin/jellyfin-web/bak/
