f4c76ac62a1352ff0589c1e4fa89746bc2bbe37e
jackloves111 <89971817+jackloves111@users.noreply.github.com>
2024-02-23 14:11:25 +0800

    【优化】增加下载错误报错的提示

--- script.sh
+++ script.sh
@@ -8,12 +8,12 @@ docker exec -it  $name rm -rf /jellyfin/jellyfin-web/jellyfin-crx/
 docker exec -it  $name mkdir -p /jellyfin/jellyfin-web/jellyfin-crx/
 
 # 下载所需文件到系统
-wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/css/style.css -O style.css  
-wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/common-utils.js -O common-utils.js  
-wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/jquery-3.6.0.min.js -O jquery-3.6.0.min.js  
-wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/md5.min.js -O md5.min.js  
-wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/content/main.js -O main.js
-echo "已下载缓存文件！"
+echo "正在下载缓存文件，请稍等... ..."
+wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/css/style.css -O style.css || { echo "错误：无法下载 style.css"; exit 1; }
+wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/common-utils.js -O common-utils.js || { echo "错误：无法下载 style.css"; exit 1; }
+wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/jquery-3.6.0.min.js -O jquery-3.6.0.min.js || { echo "错误：无法下载 style.css"; exit 1; }
+wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/static/js/md5.min.js -O md5.min.js || { echo "错误：无法下载 style.css"; exit 1; }
+wget -q --no-check-certificate https://raw.githubusercontent.com/newday-life/jellyfin-crx/master/content/main.js -O main.js || { echo "错误：无法下载 style.css"; exit 1; }
 
 # 从系统复制文件到容器内
 docker cp style.css $name:/jellyfin/jellyfin-web/jellyfin-crx/
